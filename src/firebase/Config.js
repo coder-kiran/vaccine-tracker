003e5d89c8b60003f3ea97aa51bc57764a0d269f
Kiran K K <kirankalak4@gmail.com>
2021-06-16 17:36:39 +0530

    before deployment

--- src/firebase/Config.js
+++ src/firebase/Config.js
@@ -1,14 +1,15 @@
 import firebase from 'firebase'
 
- var firebaseConfig = {
-    apiKey: "AIzaSyCLB97Qd9jZKWlwYBojtI0QOjcwThKsSxg",
-    authDomain: "vaccinetracker-56a02.firebaseapp.com",
-    projectId: "vaccinetracker-56a02",
-    storageBucket: "vaccinetracker-56a02.appspot.com",
-    messagingSenderId: "695423765813",
-    appId: "1:695423765813:web:d8195093f2d00227efd07c",
-    measurementId: "G-NHEWTY5HS8"
-  };
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+const firebaseConfig = {
+  apiKey: "AIzaSyCaTeLAOdeZBDjBpYn7cSvufzj1z7E2Ouw",
+  authDomain: "sulathavaccinetracker.firebaseapp.com",
+  projectId: "sulathavaccinetracker",
+  storageBucket: "sulathavaccinetracker.appspot.com",
+  messagingSenderId: "1025656936167",
+  appId: "1:1025656936167:web:5dd340e9801676701cb442",
+  measurementId: "G-XS9M1EETQD"
+};
 
   
  export const Firebase=firebase.initializeApp(firebaseConfig)
